aed25e6632ff7b33848b4e07d7deef9301fdaae7
XaYoZe <yang6450867@gmail.com>
2025-05-22 14:27:52 +0800

    fix: 拖動選中索引錯誤修改

--- src/demo/pages/turntableDemo.vue
+++ src/demo/pages/turntableDemo.vue
@@ -1,29 +1,22 @@
 <template>
-  <div class="turntable_demo">
-    <div class="panel_title">參數調整</div>
-    <div class="panel_content">
-      <div class="row">
-        <span>面朝前方</span><button @click="front = !front">{{ front }}</button>
-      </div>
-      <div class="row">
-        <span>可點擊</span><button @click="click = !click">{{ click }}</button>
-      </div>
-      <div class="row">
-        <span>可拖動</span><button @click="draggable = !draggable">{{ draggable }}</button>
-      </div>
+  
+<div class="turntable_demo">
+  <div class="turntable_view">
+    <div class="panel_content ctrl_box">
+      <div class="panel_title">參數調整</div>
       <div class="row">
         <span>半径: </span>
-        <input type="range" id="volume" name="volume" min="0" max="400" v-model="radius" />
+        <input type="range" id="volume"  step="1" name="volume" min="100" max="300" v-model="radius" />
         <span>{{ radius }}</span>
       </div>
       <div class="row">
         <span>上下偏移: </span>
-        <input type="range" id="volume" name="volume" min="-400" max="400" v-model="offsetY" />
+        <input type="range" id="volume"  step="1" name="volume" min="-200" max="200" v-model="offsetY" />
         <span>{{ offsetY }}</span>
       </div>
       <div class="row">
         <span>X轴角度: </span>
-        <input type="range" id="volume" name="volume" min="-90" max="90" v-model="rotateX" />
+        <input type="range" id="volume" step="1" name="volume" min="-90" max="90" v-model="rotateX" />
         <span>{{ rotateX }}</span>
       </div>
       <div class="row">
@@ -35,6 +28,15 @@
         <span>透明度: </span>
         <input type="range" v-model="opacity" max="1" min="0.1" step="0.1" />
         <span>{{ opacity }}</span>
+      </div>
+      <div class="row">
+        <span>面朝前方</span><button @click="front = !front">{{ front }}</button>
+      </div>
+      <div class="row">
+        <span>可點擊</span><button @click="click = !click">{{ click }}</button>
+      </div>
+      <div class="row">
+        <span>可拖動</span><button @click="draggable = !draggable">{{ draggable }}</button>
       </div>
       <div class="row">
         <span>过渡时长: </span>
@@ -68,8 +70,8 @@
         </div>
       </template>
     </div>
-    <div class="panel_title">效果預覽</div>
     <div class="panel_content demo_box">
+      <div class="panel_title">效果預覽</div>
       <ClientOnly>
         <Turntable
           ref="truntable"
@@ -86,6 +88,7 @@
           :autoplay="showAutoPlay && autoplay"
           :duration="duration"
           :perspective="showPerspective && perspective"
+          @change="onChange"
         >
           <div class="turntable_item" v-for="i in 10" :key="i">{{ i }}</div>
           <template #center>
@@ -94,21 +97,22 @@
         </Turntable>
       </ClientOnly>
     </div>
-    <div class="panel_title">props參數</div>
-    <div class="panel_content props_list">
-      <div class="props_item" v-if="click"><span style="color: #a6e22e">click</span></div>
-      <div class="props_item" v-if="draggable"><span style="color: #a6e22e">draggable</span></div>
-      <div class="props_item" v-if="front"><span style="color: #a6e22e">front</span></div>
-      <div class="props_item" v-if="rotateX !== 0">:<span style="color: #a6e22e">rotateX</span>="{{ rotateX }}"</div>
-      <div class="props_item" v-if="radius !== 375">:<span style="color: #a6e22e">radius</span>="{{ radius }}"</div>
-      <div class="props_item" v-if="opacity !== 1">:<span style="color: #a6e22e">scale</span>="{{ scale }}"</div>
-      <div class="props_item" v-if="opacity !== 1">:<span style="color: #a6e22e">opacity</span>="{{ opacity }}"</div>
-      <div class="props_item" v-if="offsetY !== 0">:<span style="color: #a6e22e">offsetY</span>="{{ offsetY }}"</div>
-      <div class="props_item" v-if="duration !== 300">:<span style="color: #a6e22e">duration</span>="{{ duration }}"</div>
-      <div class="props_item" v-if="showAutoPlay">:<span style="color: #a6e22e">autoplay</span>="{{ autoplay }}"</div>
-      <div class="props_item" v-if="showPerspective">:<span style="color: #a6e22e">perspective</span>="{{ perspective }}"</div>
-    </div>
   </div>
+  <div class="panel_title">props參數</div>
+  <div class="panel_content props_list">
+    <div class="props_item" v-if="click"><span style="color: #a6e22e">click</span></div>
+    <div class="props_item" v-if="draggable"><span style="color: #a6e22e">draggable</span></div>
+    <div class="props_item" v-if="front"><span style="color: #a6e22e">front</span></div>
+    <div class="props_item" v-if="rotateX !== 0">:<span style="color: #a6e22e">rotateX</span>="{{ rotateX }}"</div>
+    <div class="props_item" v-if="radius !== 375">:<span style="color: #a6e22e">radius</span>="{{ radius }}"</div>
+    <div class="props_item" v-if="opacity !== 1">:<span style="color: #a6e22e">scale</span>="{{ scale }}"</div>
+    <div class="props_item" v-if="opacity !== 1">:<span style="color: #a6e22e">opacity</span>="{{ opacity }}"</div>
+    <div class="props_item" v-if="offsetY !== 0">:<span style="color: #a6e22e">offsetY</span>="{{ offsetY }}"</div>
+    <div class="props_item" v-if="duration !== 300">:<span style="color: #a6e22e">duration</span>="{{ duration }}"</div>
+    <div class="props_item" v-if="showAutoPlay">:<span style="color: #a6e22e">autoplay</span>="{{ autoplay }}"</div>
+    <div class="props_item" v-if="showPerspective">:<span style="color: #a6e22e">perspective</span>="{{ perspective }}"</div>
+  </div>
+</div>
 </template>
 <script setup>
 import { ref, reactive } from 'vue';
@@ -136,24 +140,73 @@ const perspective = reactive({
   originY: 0,
   offsetZ: 0,
 });
+
+const onChange = (index) => {
+  console.log('onChange', index);
+};
 </script>
 
 <style lang="scss" scoped>
 .turntable_demo {
   width: 100%;
-  padding: 20px 10px 0;
+  padding: 20px 10px 0 ;
   .panel_title {
     font-size: 18px;
     font-weight: bold;
     margin-bottom: 10px;
   }
   .props_list {
+    width: 500px;
     background: #000;
     line-height: 20px;
     padding: 10px 0;
     color: #fff;
     .props_item {
       padding-left: 20px;
+    }
+  }
+  .turntable_view {
+    user-select: none;
+    display: flex;
+    position: relative;
+    .ctrl_box {
+      position: relative;
+      width: max-content;
+      z-index: 1;
+    }
+    
+    .demo_box {
+      width: 400px;
+      position: absolute;
+      left: 400px;
+      .turntable_box {
+        user-select: none;
+        .turntable_item {
+          width: 100px;
+          height: 100px;
+          background: #aed;
+          border: 3px solid #bca;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 25px;
+          flex-direction: column;
+          &.turntable_item_active {
+            background: #ace;
+          }
+          &:after, &:before  {
+            content: '';
+            flex: 1;
+            width: 1px;
+            background: #4bc;
+          }
+        }
+        .center_line {
+          width: 1px;
+          height: 250px;
+          background: #666;
+        }
+      }
     }
   }
   .panel_content {
@@ -163,7 +216,7 @@ const perspective = reactive({
       align-items: center;
       margin-bottom: 5px;
       span {
-        padding-right: 10px;
+        padding-right: 5px;
       }
       button {
         margin-left: 10px;
@@ -173,37 +226,10 @@ const perspective = reactive({
       }
       input {
         border: 1px solid #eee;
-        padding-left: 10px;
-      }
-    }
-  }
-  .demo_box {
-    position: relative;
-    .turntable_box {
-      user-select: none;
-      .turntable_item {
-        width: 100px;
-        height: 100px;
-        background: #aed;
-        border: 3px solid #bca;
-        &.turntable_item_active {
-          background: #ace;
+        width: 120px;
+        &[type="text"] {
+          padding-left: 10px;
         }
-        &:after {
-          content: '';
-          width: 1px;
-          height: 100px;
-          position: absolute;
-          left: 50%;
-          top: 50%;
-          transform: translate(-50%, -50%);
-          background: #4bc;
-        }
-      }
-      .center_line {
-        width: 1px;
-        height: 400px;
-        background: #666;
       }
     }
   }
